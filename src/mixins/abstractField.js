e72741e7bac8016859e07e0ada58fd1c7b1291b8
kevinkosterr <koster.k2001@gmail.com>
2024-06-06 19:42:08 +0200

    feat(conditionals): add ability to define conditional attributes
    Added ability to define conditional attributes, currently supported attributes are:
    - `required`
    - `disabled`

--- src/mixins/abstractField.js
+++ src/mixins/abstractField.js
@@ -1,3 +1,5 @@
+import { isFunction } from "@/helpers"
+
 export default {
 
   emits: ['onInput'],
@@ -39,8 +41,44 @@ export default {
      */
     formatFieldValue (target) {
       return target.value;
+    }
+  },
+
+  computed: {
+    /**
+     * Compute the disabled state of the field/input component. `this.field.disabled` must be set for this to work,
+     * otherwise this will always return false.
+     *
+     * You can pass a function to the `disabled` property of a field to apply the disabled state conditionally.
+     * The function will be passed the entire model, the current field instance and the field component.
+     *
+     * @returns {boolean}
+     */
+    isDisabled () {
+      if (isFunction(this.field.disabled)) {
+        return this.field.disabled.call(this, this.model, this.field, this)
+      } else {
+        return this.field.disabled || false
+      }
     },
 
-  }
+    /**
+     * Compute the required state of the field/input component. `this.field.required` must be set for this to work,
+     * otherwise this will always return false.
+     *
+     * You can pass a function to the `required` property of a field to apply the required state conditionally.
+     * The function will be passed the entire model, the current field instance and the field component.
+     *
+     * @returns {boolean}
+     */
+    isRequired () {
+      if (isFunction(this.field.required)) {
+        return this.field.required.call(this, this.model, this.field, this)
+      } else {
+        return this.field.required || false
+      }
+    }
+
+  },
 
 }
