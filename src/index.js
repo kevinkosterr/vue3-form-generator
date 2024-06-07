42a08a68f4124afa1fc6d8a74640c19dba72e114
kevinkosterr <koster.k2001@gmail.com>
2024-06-07 08:45:44 +0200

    fix(plugin): fix installation issue when no option object is passed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { setFieldComponent } from "@/helpers/fieldComponents.js";
 
 const VueFormGenerator = {
   install (app, options) {
+    if (!options) options = {}
+
     app.component('VueFormGenerator', FormGenerator)
 
-    for (const component of options.components) {
+    for (const component in options.components) {
       setFieldComponent(component.type, component.component);
     }
   }
