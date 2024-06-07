b88a56ade1c2605c86a12594c11a1f9a051b1b9e
kevinkosterr <koster.k2001@gmail.com>
2024-06-07 09:09:20 +0200

    refactor(abstractField): refactored the determination of field attribute values
    Moved logic into re-usable function.

--- src/mixins/abstractField.js
+++ src/mixins/abstractField.js
@@ -41,6 +41,20 @@ export default {
      */
     formatFieldValue (target) {
       return target.value;
+    },
+
+    /**
+     * Determine the value of a field's attribute.
+     *
+     * Will execute the method if one is passed as a property to evaluate its value.
+     *
+     * @param {String} attribute field attribute.
+     * @param {*} defaultValue default value to return if the attribute is not found.
+     * @returns {*|boolean}
+     */
+    determineDynamicAttribute (attribute, defaultValue = false) {
+      if (isFunction(this.field[attribute])) return this.field[attribute].call(this, this.model, this.field, this)
+      return this.field[attribute] === undefined ? defaultValue : this.field[attribute]
     }
   },
 
@@ -49,34 +63,26 @@ export default {
      * Compute the disabled state of the field/input component. `this.field.disabled` must be set for this to work,
      * otherwise this will always return false.
      *
-     * You can pass a function to the `disabled` property of a field to apply the disabled state conditionally.
-     * The function will be passed the entire model, the current field instance and the field component.
+     * You can pass a method to the `disabled` property of a field to apply the disabled state conditionally.
+     * The method will be passed the entire model, the current field instance and the field component.
      *
      * @returns {boolean}
      */
     isDisabled () {
-      if (isFunction(this.field.disabled)) {
-        return this.field.disabled.call(this, this.model, this.field, this)
-      } else {
-        return this.field.disabled || false
-      }
+      return this.determineDynamicAttribute('disabled')
     },
 
     /**
      * Compute the required state of the field/input component. `this.field.required` must be set for this to work,
      * otherwise this will always return false.
      *
-     * You can pass a function to the `required` property of a field to apply the required state conditionally.
-     * The function will be passed the entire model, the current field instance and the field component.
+     * You can pass a method to the `required` property of a field to apply the required state conditionally.
+     * The method will be passed the entire model, the current field instance and the field component.
      *
      * @returns {boolean}
      */
     isRequired () {
-      if (isFunction(this.field.required)) {
-        return this.field.required.call(this, this.model, this.field, this)
-      } else {
-        return this.field.required || false
-      }
+      return this.determineDynamicAttribute('required')
     }
 
   },
