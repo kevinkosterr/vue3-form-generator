0f19205c9f6ce11e8ac999d48838c95fc83944e0
kevinkosterr <koster.k2001@gmail.com>
2024-07-09 10:27:18 +0200

    refactor(helpers): move all helper functions to single file

--- src/helpers/fieldComponents.js
+++
@@ -1,8 +0,0 @@
-function getFieldComponentName (field) {
-  const fieldAttribute = field[field.inputType !== undefined ? 'inputType' : 'type']
-  return 'Field' + fieldAttribute.charAt(0).toUpperCase() + fieldAttribute.slice(1);
-}
-
-export {
-  getFieldComponentName,
-}
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,53 @@
-import isFunction from './isFunction.js'
-import { getFieldComponentName } from "./fieldComponents.js"
-import hasLabel from "./hasLabel.js"
+function isFunction(val) {
+  return typeof val === 'function';
+}
+
+function isObject(val) {
+  return typeof val === 'object';
+}
+
+function isString (val) {
+  return typeof val === 'string';
+}
+
+function toUniqueArray (arr) {
+  if (!Array.isArray(arr)) throw new Error('Argument must be of type array');
+  return Array.from(new Set(arr))
+}
+
+function getFieldComponentName (field) {
+  const fieldAttribute = field[field.inputType !== undefined ? 'inputType' : 'type']
+  return 'Field' + fieldAttribute.charAt(0).toUpperCase() + fieldAttribute.slice(1);
+}
+
+function isEmpty (value) {
+  return value === undefined || value === null || value === '';
+}
+
+function isNotEmpty (value) {
+  return !isEmpty(value);
+}
+
+function hasLabel(field) {
+  if (!field.label) return false
+
+  switch (field.type) {
+    case 'button':
+    case 'submit':
+    case 'reset':
+      return false
+    default:
+      return true
+  }
+}
 
 export {
   isFunction,
+  isObject,
+  isString,
+  toUniqueArray,
   getFieldComponentName,
-  hasLabel
+  hasLabel,
+  isEmpty,
+  isNotEmpty
 }
