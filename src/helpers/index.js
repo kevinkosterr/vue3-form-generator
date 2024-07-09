be279099720cc6c78270585d8ee2ee5c191c8362
kevinkosterr <koster.k2001@gmail.com>
2024-07-09 10:38:57 +0200

    docs: add documentation to helpers & validators

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,33 +1,73 @@
+/**
+ * Determine if value is of type Function.
+ * @param {any} val
+ * @returns {boolean}
+ */
 function isFunction(val) {
   return typeof val === 'function';
 }
 
+/**
+ * Determine if a value is of type Object.
+ * @param {any} val
+ * @returns {boolean}
+ */
 function isObject(val) {
   return typeof val === 'object';
 }
 
+/**
+ * Determine if a value is of type String.
+ * @param {any} val
+ * @returns {boolean}
+ */
 function isString (val) {
   return typeof val === 'string';
 }
 
+/**
+ * Remove duplicates from an array.
+ * @param {Array} arr
+ * @returns {Array}
+ */
 function toUniqueArray (arr) {
   if (!Array.isArray(arr)) throw new Error('Argument must be of type array');
   return Array.from(new Set(arr))
 }
 
+/**
+ * Determine the field component name based on the field schema.
+ * @param {Object} field field from form schema.
+ * @returns {string} string representation of the field component.
+ */
 function getFieldComponentName (field) {
   const fieldAttribute = field[field.inputType !== undefined ? 'inputType' : 'type']
   return 'Field' + fieldAttribute.charAt(0).toUpperCase() + fieldAttribute.slice(1);
 }
 
+/**
+ * Determine if a value is empty.
+ * @param {any} value
+ * @returns {boolean}
+ */
 function isEmpty (value) {
   return value === undefined || value === null || value === '';
 }
 
+/**
+ * Determine whether a value is not empty.
+ * @param {any} value
+ * @returns {boolean}
+ */
 function isNotEmpty (value) {
   return !isEmpty(value);
 }
 
+/**
+ * Determine if a field should have a top-level label.
+ * @param field
+ * @returns {boolean}
+ */
 function hasLabel(field) {
   if (!field.label) return false
 
--- src/validators/index.js
+++ src/validators/index.js
@@ -1,13 +1,31 @@
-import { isNotEmpty } from "@/helpers/index.js";
+import {isNotEmpty, isString} from "@/helpers/index.js";
 
 export default {
 
-  required (value, field, model, fieldComponent) {
+  /**
+   * Checks if a field meets the 'required' validation criteria.
+   *
+   * @param {any} value - The current value of the field.
+   * @param {Object} field - The field as specified in the form's schema.
+   * @param {Object} model - The form model.
+   * @param {Component} fieldComponent - Field's Vue Component.
+   * @returns {boolean} - Returns 'true' if the field is required and the value is not empty, otherwise false.
+   */
+  required(value, field, model, fieldComponent) {
     return !!(fieldComponent.isRequired && isNotEmpty(value));
   },
 
-  string (value, _field, _model, _fieldComponent) {
-    return typeof value === "string";
+  /**
+   * Checks if the field's value is of type string.
+   *
+   * @param {any} value - The current value of the field.
+   * @param {Object} field - The field as specified in the form's schema.
+   * @param {Object} model - The form model.
+   * @param {Component} fieldComponent - Field's Vue Component.
+   * @returns {boolean} - Returns 'true' if the field is required and the value is not empty, otherwise false.
+   */
+  string(value, field, model, fieldComponent) {
+    return isString(value);
   },
 
 
