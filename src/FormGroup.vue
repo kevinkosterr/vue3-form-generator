6babf92d84148ba98f45d7d6cb408ffd3295ffe2
kevinkosterr <koster.k2001@gmail.com>
2024-09-26 20:13:59 +0200

    fix: label on checkbox showing twice
    Fixed an issue where checkboxes had two labels if `type` was specified to be `checkbox`, instead of it being assigned to `inputType`

--- src/FormGroup.vue
+++ src/FormGroup.vue
@@ -40,7 +40,7 @@ const fieldId = computed(() => {
 
 const shouldHaveLabel = computed(() => {
   // checkbox will have their own label
-  return hasLabel(props.field) && props.field.inputType !== 'checkbox'
+  return hasLabel(props.field) && ![ props.field.inputType, props.field.type ].includes('checkbox')
 })
 </script>
 
