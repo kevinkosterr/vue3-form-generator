fa862c0baaaddf6eb3db946194d1612fad3855ff
kevinkosterr <koster.k2001@gmail.com>
2024-09-26 21:56:14 +0200

    test: add docs to util functions

--- __tests__/_resources/utils.js
+++ __tests__/_resources/utils.js
@@ -2,10 +2,26 @@ import { mount } from '@vue/test-utils'
 
 import FormGenerator from '@/FormGenerator.vue'
 
+/**
+ * Mount the form generator component
+ * @param schema - schema object to pass as prop
+ * @param {Object} model - model object to pass as prop
+ * @returns {VueWrapper<FormGenerator>}
+ */
 export function mountFormGenerator (schema, model) {
   return mount(FormGenerator, { props: { schema, model } })
 }
 
+/**
+ * Generate a form schema for a single field component
+ * @param {String} name - name of the field
+ * @param {String} model - model key of the field
+ * @param {String} type - field type
+ * @param {String} inputType - field input type, required if `type === 'input'`
+ * @param {String} label - field label
+ * @param {any} initialValue - initial model value
+ * @returns {{schema: {fields: [{name, model, inputType, label, type}]}, model: {}}}
+ */
 export function generateSchemaSingleField (name, model, type, inputType, label, initialValue) {
   return {
     model: {
@@ -21,6 +37,11 @@ export function generateSchemaSingleField (name, model, type, inputType, label, 
   }
 }
 
+/**
+ * Generate props for a single field component
+ * @param {Object} formSchema - entire form schema object
+ * @returns {{field: *, model, id: string, formGenerator: {}}}
+ */
 export function generatePropsSingleField (formSchema) {
   return {
     id: formSchema.name + '_test_id',
