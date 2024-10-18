91ac0d4fb15948bb09e9e0a55a02e64eebc26457
kevinkosterr <koster.k2001@gmail.com>
2024-10-18 13:00:59 +0200

    chore(release): re-order semantic-release plugins

--- release.config.mjs
+++ release.config.mjs
@@ -7,12 +7,10 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
     [ '@semantic-release/git', {
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     } ],
-    '@semantic-release/npm',
-    [ '@semantic-release/github', {
-      assets: [ { path: 'dist/*', label: 'Distribution Files' } ]
-    } ]
+    '@semantic-release/github'
   ]
 }
