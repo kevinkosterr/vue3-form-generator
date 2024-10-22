b70f72572632bdae1db6ded95c4af369225481b1
kevinkosterr <koster.k2001@gmail.com>
2024-10-22 20:47:19 +0200

    chore(release): remove git plugin

--- release.config.mjs
+++ release.config.mjs
@@ -8,9 +8,6 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    [ '@semantic-release/git', {
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-    } ],
     '@semantic-release/github'
   ]
 }
