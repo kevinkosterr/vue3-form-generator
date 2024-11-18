4d567c443477123ffc4948eeef8bd05b22d1e696
kevinkosterr <koster.k2001@gmail.com>
2024-11-18 16:31:25 +0100

    refactor(field-checkbox): refactor to Composition API

--- src/fields/input/FieldCheckbox.vue
+++ src/fields/input/FieldCheckbox.vue
@@ -12,16 +12,41 @@
   <label v-if="field.label" style="margin-left: .4em" :for="id"> {{ field.label }}</label>
 </template>
 
-<script>
-import { abstractField } from '@/mixins'
+<script setup>
+import { toRefs } from 'vue'
+import {
+  useModel,
+  useAttributes,
+  useValidate,
+  useFieldProps,
+  useFieldEmits
+} from '@/composables'
 
-export default {
-  name: 'FieldCheckbox',
-  mixins: [ abstractField ],
-  methods: {
-    formatFieldValue (target) {
-      return target.checked
-    }
-  }
+const emits = defineEmits(useFieldEmits())
+const props = defineProps(useFieldProps())
+
+const { field, model } = toRefs(props)
+
+const { currentModelValue } = useModel(model.value, field.value)
+const { isRequired, isDisabled } = useAttributes(model.value, field.value)
+const { errors, validate } = useValidate(
+  model.value,
+  field.value,
+  isDisabled.value,
+  isRequired.value,
+  false,
+  currentModelValue.value
+)
+
+const onFieldValueChanged = ({ target }) => {
+  errors.value = []
+  validate().then((validationErrors) => {
+    emits('validated',
+      validationErrors.length === 0,
+      validationErrors,
+      field.value
+    )
+  })
+  emits('onInput', target.checked)
 }
 </script>
