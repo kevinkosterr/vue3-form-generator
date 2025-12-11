517eb8f176f2a329927434ed56e670051d7264ea
kevinkosterr <koster.k2001@gmail.com>
2025-12-11 20:20:35 +0100

    chore: run tsc on build

--- src/composables/useLabelIcon.ts
+++ src/composables/useLabelIcon.ts
@@ -1,24 +1,24 @@
-import { type ComputedRef, computed, type ComponentPublicInstance } from 'vue'
+import { type ComputedRef, computed, type Component } from 'vue'
 import type { LabelIconDefinition } from '@/resources/types/field/base'
 
 /**
  * Simple composable that determines which icon to display and where. If there's no definition for an icon
  * @param iconDefinition - field schema icon definition.
  */
-export function useLabelIcon(iconDefinition: string | LabelIconDefinition | ComponentPublicInstance | undefined) {
+export function useLabelIcon(iconDefinition: string | LabelIconDefinition | Component | undefined) {
 
   /**
    * The icon that should be displayed with a label.
    * If no icon is defined, return null.
    */
-  const labelIcon: ComputedRef<string | ComponentPublicInstance | null> = computed(() => {
+  const labelIcon: ComputedRef<string | Component | null> = computed(() => {
     if (!iconDefinition) return null
 
     if (iconDefinition.hasOwnProperty('icon')) {
       return (iconDefinition as LabelIconDefinition).icon
     }
 
-    return (iconDefinition as ComponentPublicInstance | string)
+    return (iconDefinition as Component | string)
   })
 
   /**
